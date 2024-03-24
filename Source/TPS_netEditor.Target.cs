ad9f77c2893da0f80302631f21cb6b2a7a4c62a4
Alexey <nublin1@yandex.ru>
2024-03-24 22:26:32 +0300

    wip

---
+++ Source/TPS_netEditor.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class TPS_netEditorTarget : TargetRules
+{
+	public TPS_netEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+
+		ExtraModuleNames.AddRange( new string[] { "TPS_net" } );
+	}
+}
