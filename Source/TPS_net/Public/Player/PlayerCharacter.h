4dbbcb69890fdd111d020cecc85a99777ed0ab9a
Alexey <nublin1@yandex.ru>
2024-03-25 16:11:56 +0300

    wip

---
+++ Source/TPS_net/Public/Player/PlayerCharacter.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class TPS_NET_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	APlayerCharacter();
+
+protected:
+	// Properties
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool IsAiming;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CameraInterpolationSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector DefaultCameraLocation;
+	
+	//Functions
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
